88a2d1ffb2bcc86776f539512aa248e18e13c0fd
Alexander Nguyen <alng6160@colorado.edu>
2024-12-05 12:37:42 -0700

    insert.sql login functional

--- ProjectSourceCode/init_data/insert.sql
+++ ProjectSourceCode/init_data/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (username, password, name, email, gender, age)
 VALUES
-('test1', 'password1', 'name1', 'test1@gmail.com', 'M', 1),
-('test2', 'password2', 'name2', 'test2@gmail.com', 'F', 2);
+('test1', '$2a$10$0nuiAcxmpycVE73Ilf.dHep/XZvy1/v3/iy6eZFw.xWUChCS1xMZ.', 'name1', 'test1@gmail.com', 'M', 1),
+('test2', '$2a$10$iLkX7nwuNYFlaxpleHZ5dOyEdzFFARQEpdfDRjYS8qSwX1EmOPbR.', 'name2', 'test2@gmail.com', 'F', 2);
 
 INSERT INTO teams (team_id, team_name, created_at)
 VALUES
